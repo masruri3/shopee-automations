363646e67802f6bf1d8521acdc6ce70002b180a3
Muhammad Zahid Masruri <masruri3@gmail.com>
2020-05-08 00:28:44 +0700

    added api to automatic get csrf token

--- shoppe_autosub.js
+++ shoppe_autosub.js
@@ -1,26 +1,26 @@
 var productURL = "";
-var csrftoken = "";
+var csrftoken = getCookie("csrftoken");
 var author_shopid;
 var author_username;
 var itemid;
 var shopid;
 var max = 4;
 
-itemid = productURL.slice(productURL.lastIndexOf(".")+1, productURL.length);
-productURLSliced = productURL.slice(0, productURL.lastIndexOf(".")-1);
-shopid = productURL.slice(productURLSliced.lastIndexOf(".")+1, productURL.lastIndexOf("."));
+itemid = productURL.slice(productURL.lastIndexOf(".") + 1, productURL.length);
+productURLSliced = productURL.slice(0, productURL.lastIndexOf(".") - 1);
+shopid = productURL.slice(productURLSliced.lastIndexOf(".") + 1, productURL.lastIndexOf("."));
 
-for(var i = 0; i < max; i++){
+for (var i = 0; i < max; i++) {
     fetch("https://shopee.co.id/api/v2/item/get_ratings?filter=0&flag=1&itemid=" + itemid + "&limit=1&offset=" + i + "&shopid=" + shopid + "&type=0")
-    .then(response => response.json())
-    .then(data => {
-        author_shopid = data.data.ratings[0].author_shopid;
-        author_username = data.data.ratings[0].author_username;   
-        shopee_subscribe();
+        .then(response => response.json())
+        .then(data => {
+            author_shopid = data.data.ratings[0].author_shopid;
+            author_username = data.data.ratings[0].author_username;
+            shopee_subscribe();
         })
 }
 
-function shopee_subscribe(){
+function shopee_subscribe() {
     console.log("Data Parser" + author_shopid + author_username);
     var shopee_api_subs = "https://shopee.co.id/api/v0/buyer/follow/shop/" + author_shopid + "/";
     fetch(shopee_api_subs, {
@@ -43,4 +43,20 @@ function shopee_subscribe(){
         "mode": "cors",
         "credentials": "include"
     });
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
